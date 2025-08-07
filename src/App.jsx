4acad72f00db9550680334f682742acf2e8533a8
Ignacio <ignacionazar9@gmail.com>
2025-08-07 16:46:40 -0300

    solicite el objeto a la api del clima y lo guarde en el state clima

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
-import Formulario from "./components/Formulario"
-import Titulo from "./components/Titulo"
-import "bootstrap/dist/css/bootstrap.min.css"
+import { useEffect, useState } from "react";
+import Formulario from "./components/Formulario";
+import Titulo from "./components/Titulo";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const [clima, setClima] = useState({});
+
+  useEffect(() => {
+    obtenerClima();
+  }, []);
+
+  const obtenerClima = async () => {
+    const respuesta = await fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=Buenos Aires,AR&units=metric&lang=es&appid=6d0181333345780305a81906090f0e2a"
+    );
+    console.log(respuesta)
+    if(respuesta.status === 200){
+      const datos = await respuesta.json()
+      console.log(datos)
+      //guardar en el state clima
+      setClima(datos)
+    }
+  };
 
   return (
     <>
-    <Titulo></Titulo>
-    <Formulario></Formulario>
+      <Titulo></Titulo>
+      <Formulario></Formulario>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
